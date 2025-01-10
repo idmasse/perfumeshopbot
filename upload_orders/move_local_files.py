55e9ba770484b09732d2dafe591c4d46469c29d4
Ian Masse <ianm@Ians-MacBook-Pro.local>
2025-01-10 13:49:16 -0800

    updates order uploads

--- upload_orders/move_local_files.py
+++ upload_orders/move_local_files.py
@@ -10,14 +10,26 @@ def move_files_to_processed(directory, processed_directory, file_list=None):
         logger.info(f'created "processed" directory at: {processed_directory}')
 
     if file_list is None:
-        file_list = os.listdir(directory) # list all the files in directory
+        file_list = os.listdir(directory)
+
+    logger.debug(f"moving files: {file_list}")
+    logger.debug(f"from: {directory}")
+    logger.debug(f"to: {processed_directory}")
 
     for filename in file_list:
         file_path = os.path.join(directory, filename)
+        destination = os.path.join(processed_directory, filename)
+        
+        logger.debug(f"Source exists: {os.path.exists(file_path)}")
+        logger.debug(f"Have write permission: {os.access(processed_directory, os.W_OK)}")
+        
         if os.path.isfile(file_path):
             try:
                 destination = os.path.join(processed_directory, filename)
                 shutil.move(file_path, destination)
-                logger.info(f"moved file '{filename}' to 'processed' directory")
+                logger.info(f"Moved file '{filename}' to '{destination}'")
             except Exception as e:
-                logger.error(f"failed to move file '{filename}': {e}")
+                logger.error(f"Failed to move '{filename}': {str(e)}", exc_info=True)
+
+if __name__ == '__main__':
+    move_files_to_processed()
