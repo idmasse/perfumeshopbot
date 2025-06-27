a3ab6c84d00d6cc892ebce83fac37a636c303021
idmasse <idmasse@icloud.com>
2025-06-27 16:48:12 -0700

    updates chrome options

--- utils/selenium_setup.py
+++ utils/selenium_setup.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 def get_headless_driver():
     logger.info("Initializing Chrome driver...")
     chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
